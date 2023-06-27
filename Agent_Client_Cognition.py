0a25064804075564a7da264e743c10c1cb22cb1c
FusiDaniel <fpires.daniel@gmail.com>
2023-06-26 22:22:47 -0300

    first tests

--- Agent_Client_Cognition.py
+++ Agent_Client_Cognition.py
@@ -16,6 +16,38 @@ from Agent_Client_Setup import keyMagACT, keyMagMOV, keyMagREQ, keyMagROT, ACTgr
     OUTcnt, OUTdie, OUTgrb, OUTnon, OUTrst, OUTsuc, SRVcnn, SRVinv, SRVnor, SRVpsd, \
     DIRn, DIRne, DIRe, DIRse, DIRs, DIRsw, DIRw, DIRnw
 
+# fazer um preprocessador, que adiciona 10 ao valor
+# das entradas para separar os estados de com e sem ouro
+def move():
+    return []
+def move_foward():
+    return []
+def rotate():
+    return []
+def danger():
+    return []
+def certain_death():
+    return []
+def exit(state):
+    if state == 'with_gold':
+        return move_foward()
+    else:
+        return move()
+
+# sense: [m_foward, rot_left, rot_right, grab, leave]
+moves = [
+    move,
+    move,
+    danger,
+    move_foward,
+    move_foward,
+    exit,
+    rotate,
+    move,
+    move_foward,
+    move_foward,
+    move,
+]
 
 # este método é usado para 'analisar a resposta/feedback' recebido do EnviSim
 def feedback_analysis(vecInpSens: np.int32, carryRWD: int) -> int:
@@ -45,23 +77,29 @@ def feedback_analysis(vecInpSens: np.int32, carryRWD: int) -> int:
 def infer(vecInpSens: np.int32) -> int:
     print('infer: ', len(vecInpSens), ' ', vecInpSens)
     outy = -1  # por default, o índice de saída é um índice de erro
-
+    indx_outs = [3, 3, 3, 3, 11, 11]
+    
     if len(vecInpSens) == 1:  # * quando ocorreu apenas 1 requisição de informação
         if np.sum(vecInpSens) == 0 :  # se num_input_bits for zero
             return outy  # retorna erro (-1)
         else:
-            indx_outs = [0, 1, 3, 3, 3, 11, 12, 13]
-            outy = np.random.choice(indx_outs)  # AGENTE TONTO: escolhe randomicamente 1 saída
-            print('out: ', OutNeurons[outy])
+            print(vecInpSens[0][0])
+            if (vecInpSens[0][0] == 1 or vecInpSens[0][1] == 1 or vecInpSens[0][7] == 1):
+                outy = np.random.choice(indx_outs)
+
+            else:
+                outy = 11
 
     elif len(vecInpSens) > 1:  # * quando várias solicitações de informações foram feitas, len(vecInpSens) > 1
         for k in range(len(vecInpSens)):  # lembrar de varrer todas as entradas
             if np.sum(vecInpSens[k]) != 1:  # se num bits input é '!=1
                 return outy  # retorna erro (-1)
             else:
-                indx_outs = [0, 1, 3, 3, 3, 11, 12, 13]
-                outy = np.random.choice(indx_outs)  # AGENTE TONTO: escolhe randomicamente 1 saída
-                print('out: ', OutNeurons[outy])
+                print(k[0])
+                if (k[0] == 1 or k[1] == 1 or k[7] == 1):
+                    outy = np.random.choice(indx_outs)
+                else:
+                    outy = 11
 
     else:  # se não com array vecInpSens vazio, len() = 0
         return outy  # retorna erro (-1)
